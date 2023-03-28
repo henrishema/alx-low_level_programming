ab21b3a34d644f9ef36f5d1dceadc138e0a22d21
henrishema <shemahen00@gmail.com>
2023-03-28 03:20:50 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * _isupper - function to determine upper case
+ *
+ * Return: 1 if upper and 0 if not
+ */
+int _isupper(int c)
+{
+	if (c == 'A')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
