cbf85f2eeb594ca44195f14de9f4dd827b95d835
henrishema <125571139+henrishema@users.noreply.github.com>
2023-04-19 23:19:36 +0200

    main

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,19 +1,12 @@
-#include "main.h"
-/**
- *malloc_checked - array for prints a string
- *@b: number of memory
- *Return: void
- */
-
-void *malloc_checked(unsigned int b)
-{
-void *p;
-
-p = malloc(b);
-if (p == NULL)
-{
-exit(98);
-}
-
-return (p);
-}
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmedb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+#endif
